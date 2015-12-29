75e5baf177276d25f4b5aedf0530301d807d2b51
baron-g8hub <lugtubaron@gmail.com>
2015-12-29 15:59:53 +0800

    Test new controller

---
+++ MaynoothFloristSite/Controllers/TesterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MaynoothFloristSite.Controllers
+{
+    public class TesterController : Controller
+    {
+        // GET: Tester
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
